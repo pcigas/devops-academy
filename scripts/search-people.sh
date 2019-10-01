0e4531add3bc418d9a3b911522aa54d971b7e5f2
Patrik Cigas <pato.cigas@gmail.com>
2019-10-01 13:04:43 +0200

    Search-people improved

--- scripts/search-people.sh
+++ scripts/search-people.sh
@@ -4,17 +4,45 @@ PEOPLE=("Jozko Mrkvicka" \
 "Bruno Banani" \
 "Milan Rufus")
 
-echo "Zadaj koho hladas"
-read choose
-
-for name in "${PEOPLE[@]}"
-do
-	if [[ $name == *"$choose"* ]]
+search()
+{
+	name="$@"
+	local matches="0"
+	for name in "${PEOPLE[@]}"
+	do
+		if [[ $name == *"$choose"* ]]
+		then
+			echo "Match! -> $name"
+			matches=$((matches+1))
+		fi
+	done
+	
+	if [[ $matches -gt 0 ]]
 	then
-		echo "Match! -> $name"
-		exit 0
+		return 0
+	else
+		return 1
 	fi
-done
+}
 
-echo "No match"
-exit 1
+read_name()
+{
+	if [[ $# -eq 0 ]]
+	then
+		echo "Zadaj koho hladas"
+		read choose
+	else
+		choose="$@"
+	fi
+}
+
+read_name "$@"
+
+if search $choose
+then
+	echo "Found!"
+else
+	echo "Not found!"
+fi
+	
+
